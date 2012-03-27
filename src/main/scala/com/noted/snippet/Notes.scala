3bf520c5dac4b070f38c35841b9a0ae4fdf17d60
Rajesh <rajesh@rajeshpg.com>
2012-03-27 16:33:08 +0530

    ajaxify delete notes

--- src/main/scala/com/noted/snippet/Notes.scala
+++ src/main/scala/com/noted/snippet/Notes.scala
@@ -2,53 +2,46 @@ package com.noted {
   package snippet {
 
     import scala.xml.NodeSeq
-  import scala.xml.Unparsed
-  import net.liftweb.util.Helpers
-  import scala.xml.Text
-  import net.liftweb.http.SHtml
-  import Helpers._
-  import net.liftweb.http.RequestVar
-  import com.noted.model._
-  import net.liftweb.http.S
-  import net.liftweb.common._
-  import net.liftweb.http.js.JsCmd
-  import com.noted.comet.NotesServer
-    
+    import scala.xml.Unparsed
+    import net.liftweb.util.Helpers
+    import scala.xml.Text
+    import net.liftweb.http.SHtml
+    import Helpers._
+    import net.liftweb.http.RequestVar
+    import com.noted.model._
+    import net.liftweb.http.S
+    import net.liftweb.common._
+    import net.liftweb.http.js.JsCmd
+    import com.noted.comet.NotesServer
+
     object Notes {
 
       def noteForm(in: NodeSeq): NodeSeq = {
-    		  println("Notes snippet noteform")
         bind("f", in,
-          "desc" -%> SHtml.text(note.description.is,note.description(_)),
+          "desc" -%> SHtml.text(note.description.is, note.description(_)),
           "submit" -%> SHtml.submit("Note It !", add)) ++ SHtml.hidden(add _)
       }
 
-     
- 
       def delete(note: Note): JsCmd = {
         println("delete")
         note.delete_!
         NotesServer ! "refresh"
-        S.error("descError","note deleted")
+        S.error("descError", "note deleted")
       }
 
       def add() = {
-        
-        println("note desc is "+note.description)
         if (note.description.isEmpty()) {
-          S.error("descError","Please enter a note")
+          S.error("descError", "Please enter a note")
         } else {
           note.save
           NotesServer ! "refresh"
         }
 
       }
-      
-
 
     }
-    
-    object note extends RequestVar[Note]({Note.create})
+
+    object note extends RequestVar[Note]({ Note.create })
 
   }
 }
